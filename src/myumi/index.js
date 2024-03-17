847452f1132c55b004d7f26364fbb4acd8940128
bysking <bysking@qq.com>
2024-03-17 12:47:52 +0800

    feat: 实现自定义渲染路由列表

--- src/myumi/index.js
+++ src/myumi/index.js
@@ -4,4 +4,32 @@ import { Router, Route, Switch } from "react-router-dom";
 import history from "./core/history";
 import { getRoutes } from "./core/routes";
 
+let routes = getRoutes();
+
+function renderRoutes(routesList) {
+  return routesList.map(
+    ({ path, exact, component: RouterCoponent, routes: childrenRoutes }) => {
+      return (
+        <Route
+          path={path}
+          exact={exact}
+          render={(routProps) => {
+            {
+              /* 3种方式渲染： component, render(支持传参), children */
+            }
+            return <RouterCoponent {...routProps} />;
+          }}
+        >
+          {/* 递归处理 */}
+          <Switch>{renderRoutes(childrenRoutes)}</Switch>
+        </Route>
+      );
+    }
+  );
+}
+
+const App = () => {
+  <Router history={history}>{renderRoutes(routes)}</Router>;
+};
+
 ReactDom.render(<App />, document.getElementById("root"));
